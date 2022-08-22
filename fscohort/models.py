323f6f45961f36a7ac3d22dd1bfc09454e29468a
serdalaydin6116 <serdalaydin.6116@gmail.com>
2022-08-22 22:19:05 +0300

    commit3

--- fscohort/models.py
+++ fscohort/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Student(models.Model):
+    first_name = models.CharField('Adı', max_length=50)
+    last_name = models.CharField('Soyadı', max_length=50)
+    number = models.IntegerField('Numara')
+    about= models.TextField('Hakkında', blank=True, null=True)
+    avatar = models.ImageField('Resim', blank=True, null=True, upload_to='media/')
+
+    def __str__(self):
+        return f"{self.number} - {self.first_name} {self.last_name}"
+
+    # https://docs.djangoproject.com/en/4.1/ref/models/options/#model-meta-options
+    class Meta:
+        ordering = ['number'] # DESC -> ['-number']
+        verbose_name_plural = 'Öğrenciler'
